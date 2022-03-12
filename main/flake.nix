7b3a947bddb041791adb3f7ff831af2d0b9e2686
hugosenari <hugosenari@gmail.com>
2022-03-12 04:34:28 -0300

    chore: re index nordaudio

---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A small wrapper around PortAudio for cross-platform audio IO.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nordaudio-main.flake = false;
+  inputs.src-nordaudio-main.owner = "Psirus";
+  inputs.src-nordaudio-main.ref   = "refs/heads/main";
+  inputs.src-nordaudio-main.repo  = "nordaudio";
+  inputs.src-nordaudio-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nordaudio-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nordaudio-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
